dc22617d7bbf2ab3ba7af44962ee63c71e78542c
parvez76 <parvez3676@gmail.com>
2024-07-13 19:23:06 +0600

    enabled cluster endpoint public access

--- EKS/main.tf
+++ EKS/main.tf
@@ -30,11 +30,11 @@ module "vpc" {
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.24"
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     node = {
